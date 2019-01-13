f882a273e1ccba3162ba24c640b9fbf8d96a530b
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2019-01-14 00:14:37 +0100

    added tests

---
+++ FitnessAppTests/Services/Calculators/BMRCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using FitnessApp.Services.Implementations.Calculators;
+using FitnessApp.Services.Interfaces;
+using Models.ServiceModels.Calculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace FitnessAppTests.Services.Calculators
+{
+    public class BMRCalculatorTests
+    {
+        [Fact]
+        public void CalculateTest()
+        {
+            ICalculator calculator = new BMRCalculator();
+            CalculatorModel model = new CalculatorModel();
+            model.Age = 1;
+            model.Gender = Models.Enums.GenderEnum.Gender.Man;
+            model.Height = 1;
+            model.Waist = 1;
+            model.Weight = 1;
+            model.Hip = 1;
+            var result = calculator.Calculate(model);
+            Assert.True(result.BMRResult > 0);
+        }
+    }
+}
---
+++ FitnessAppTests/Services/Calculators/WHRCalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using FitnessApp.Services.Implementations.Calculators;
+using FitnessApp.Services.Interfaces;
+using Models.ServiceModels.Calculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FitnessAppTests.Services.Calculators
+{
+    public class WHRCalculatorTests
+    {
+        [Fact]
+        public void CalculateTest()
+        {
+            ICalculator calculator = new WHRCalculator();
+            CalculatorModel model = new CalculatorModel();
+            model.Age = 1;
+            model.Gender = Models.Enums.GenderEnum.Gender.Man;
+            model.Height = 1;
+            model.Waist = 1;
+            model.Weight = 1;
+            model.Hip = 1;
+            var result = calculator.Calculate(model);
+            Assert.True(result.WHRResult > 0);
+        }
+    }
+}
