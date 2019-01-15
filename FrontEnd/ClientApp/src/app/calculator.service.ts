9e6748446ea64481c516eab1e7a9bf421670d694
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2019-01-16 00:12:26 +0100

    added cal service

---
+++ FrontEnd/ClientApp/src/app/calculator.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+export interface Calculator {
+  weight: number;
+  height: number;
+}
+
+export interface CalculatorResult {
+  BMIResult: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CalculatorService {
+  constructor(private http: HttpClient) { }
+
+  getBMIResult(cal: Calculator): Observable<CalculatorResult> {
+
+    const params = new HttpParams()
+      .set('weight', cal.weight.toString())
+      .set('height', cal.height.toString());
+    return this.http.get<CalculatorResult>('http://localhost:44369/api/calculator/bmi', {params});
+  }
+}
