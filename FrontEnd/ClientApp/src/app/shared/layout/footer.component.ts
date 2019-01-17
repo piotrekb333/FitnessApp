9f7ba67296f206d713aee791851997a343bf227c
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2019-01-17 21:22:17 +0100

    added validation

--- FrontEnd/ClientApp/src/app/shared/layout/footer.component.ts
+++ FrontEnd/ClientApp/src/app/shared/layout/footer.component.ts
@@ -1,26 +1,50 @@
 import { Component } from '@angular/core';
 import { NewsletterService, Newsletter } from 'src/app/shared/services/newsletter.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+declare var jquery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-layout-footer',
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
   today: number = Date.now();
+  registerForm: FormGroup;
+  submitted = false;
   public newsletterModel: Newsletter;
   constructor(
-    private newsletterService: NewsletterService
+    private newsletterService: NewsletterService,
+    private formBuilder: FormBuilder
   ) { }
   ngOnInit() {
     this.newsletterModel = {} as Newsletter;
+    this.registerForm = this.formBuilder.group({
+      txtNewsletter: ['', [Validators.required, Validators.email]],
+    });
   }
-
+  get f() {
+    return this.registerForm.controls;
+  }
   async SubscribeNewsletter() {
+    debugger;
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
     await this.newsletterService
       .postNewsletter(this.newsletterModel)
       .subscribe(data => {
-        console.log("POST Request is successful ", data);
+        $.alert({
+          title: 'Sukces!',
+          content: 'Zapisano pomyślnie',
+        });
       },
-        error => {
+      error => {
+        $.alert({
+          title: 'Błąd!',
+          content: 'Wystąpił błąd!',
+        });
           console.log("Error", error);
       });
   }
