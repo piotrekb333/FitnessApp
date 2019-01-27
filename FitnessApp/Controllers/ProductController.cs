4028255adc1f53797ec5dc01a8312775b1a338f8
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2019-01-27 23:17:47 +0100

    added product controller

---
+++ FitnessApp/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using FitnessApp.Services.Interfaces;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Models.ServiceModels.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Models.Enums.ResultEnum;
+
+namespace FitnessApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/product")]
+    [EnableCors("MyPolicy")]
+    public class ProductController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductService _productService;
+        public ProductController(IMapper mapper, IProductService productService)
+        {
+            _mapper = mapper;
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(AddProductModel model)
+        {
+            var result = _productService.AddProduct(model);
+            if (result == ServiceResult.Ok)
+                return Ok();
+            else
+                return BadRequest();
+
+        }
+
+        [HttpPut]
+        public IActionResult Put(UpdateProductModel model)
+        {
+            var result = _productService.UpdateProduct(model);
+            if (result == ServiceResult.Ok)
+                return Ok();
+            else if (result == ServiceResult.NotFound)
+                return NoContent();
+            else
+                return BadRequest();
+
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var result = _productService.DeleteProduct(id);
+            if (result == ServiceResult.Ok)
+                return Ok();
+            else if (result == ServiceResult.NotFound)
+                return NoContent();
+            else
+                return BadRequest();
+
+        }
+    }
+}
